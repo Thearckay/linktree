88d19358ea065617e7ce0c3418cd0501c5ef3e45
Thearckay <thearckay@gmail.com>
2025-05-30 00:45:23 -0300

    Linktree
    Implementação visual no botão correspondente ao tema selecionado;
    
    Correção de bug de click - Antes nem todos os clicks funcionavam devido ao tamanho do padding que executa a função de mudar o tema *Corrigido

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 // função que muda o tema
 function changeLightMode() {
+    console.log('clicado')
     const htmlElement = document.documentElement
-    htmlElement.classList.toggle('dark')
     const img = document.getElementById('profilePic')
+    const switchToggleTheme = document.getElementById('lightSwitch')
+
+    htmlElement.classList.toggle('dark')
 
     if(htmlElement.classList.contains('dark')){
         img.setAttribute('src', 'img/pic-dark.png')
+        switchToggleTheme.classList.remove('bi-brightness-high')
+        switchToggleTheme.classList.add('bi-moon-stars')
 
     } else {
         img.setAttribute('src', 'img/pic-light.png')
+        switchToggleTheme.classList.remove('bi-moon-stars')
+        switchToggleTheme.classList.add('bi-brightness-high')
+
     }
 }
